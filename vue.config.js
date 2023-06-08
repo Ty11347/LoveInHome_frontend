a3e53b4bce33724f93a0ef485af9c07e2e32cf8e
edvic <edvic113@gmail.com>
2023-06-08 13:14:06 +0800

    edit port to 3000, auto open page when run serve - v2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,33 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+
+  publicPath: './',
+  lintOnSave: false,
+  productionSourceMap: false,//防止打包后泄漏源码
+  devServer: {
+    port: 3000,
+    open: true,
+    // overlay: {
+    //   warnings: false,
+    //   errors: true
+    // },
+    proxy: {
+      // '/api': {
+      //     // 本地服务接口地址
+      //     target: 'http://10.82.1.111:5000/',
+      //     ws: true,
+      //     logLevel: 'debug',
+      //     // pathRewrite: {
+      //     //     '^/dev-api': '/'
+      //     // }
+      // },
+      // '/api': {
+      //   // 本地服务接口地址
+      //   target: 'http://221.193.236.253:8060/',
+      //   ws: true,
+      //   logLevel: 'debug',
+      // pathRewrite: {
+      //     '^/api': '/api'
+      // }
+    },
+  }
+}
